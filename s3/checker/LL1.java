83382cbd383c2799be2dbbea87446d8adaffba7a
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2024-12-05 12:52:44 +0900

    Changing LL1.java

--- s3/checker/LL1.java
+++ s3/checker/LL1.java
@@ -67,10 +67,20 @@ public class LL1 {
     }
     
     /**
-     * 識別子名を登録するメソッド
+     * 識別子名を取得するメソッド
      * 
      */
+    public String getLexical(int lineNum) {
+    	return tokens.get(lineNum).get(LEXERICALCOLS);
+    }
     
+    /**
+     * トークンを取得するメソッド
+     * 
+     */
+    public String getToken(int lineNum) {
+    	return tokens.get(lineNum).get(TOKENCOLS);
+    }
     
     /**
      * プログラム名
@@ -120,8 +130,14 @@ public class LL1 {
     	// 変数名の並び
     	VariableNameGroup variableNameGroup = variableNameGroup();
     	
+    	// "SCOLON"の判定
+    	checkToken("SCOLON")
+    	
     	// 型
-    	Type type = new type();
+    	Type type = type();
+    	
+    	// "SSEMICOLON"の判定
+    	checkToken("SSEMICOLON");
     	
     	return new VariableDeclarationGroup(variableNameGroup, type);
     }
@@ -143,9 +159,100 @@ public class LL1 {
      */
     public VariableName variableName() throws SyntaxException {
     	// 識別子の判定
+    	if (isToken("SIDENTIFIER")) {
+    		return new VariableName(getLexical(tokenIndex));
+    	} else {
+    		e.throwError(tokenIndex);
+    		return null;
+    	}    	
+    }
+    
+    /**
+     * 型
+     * 
+     */
+    public Type type() throws SyntaxException {
+    	// 標準型の判定
+    	GeneralType generalType = generalType();
     	
+    	// 配列型の判定
+    	ArrayType arrayType = arrayType();
     	
-    	return new VariableName();
+    	return new Type(generalType, arrayType);
+    }
+    
+    /**
+     * 標準型
+     * 
+     */
+    public GeneralType generalType() throws SyntaxException {
+    	if (isToken("SINTEGER") || isToken("SCHAR") || isToken("SBOOLEAN")) {
+    		return new GeneralType(getToken(tokenIndex));
+    	}
+    	
+    	return null;
+    }
+    
+    /**
+     * 配列型
+     * 
+     */
+    public ArrayType arrayType() throws SyntaxException {
+    	// "SARRAY"の判定
+    	if (!isToken("SARRAY")) {
+    		e.throwError(tokenIndex);
+    		return null;
+    	}
+    	
+    	// 添え字の最小値
+    	Integer minimumIndex = integer();
+    	
+    	// "SDOT"の判定
+    	if (!isToken("SDOT")) {
+    		e.throwError(tokenIndex);
+    		return null;
+    	}
+    	
+    	// 添え字の最大値
+    	Integer maximumIndex = integer();
+    	
+    	// "SOF"
+    	if (!isToken("SOF")) {
+    		e.throwError(tokenIndex);
+    		return null;
+    	}
+    	
+    	// 標準型
+    	GeneralType generalType = generalType();
+    	
+    	return new ArrayType(minimumIndex, maximumIndex, generalType);
+    }
+    
+    /**
+     * 整数
+     * 
+     */
+    public Integer integer() throws SyntaxException {
+    	// 符号
+    	Sign sign = sign(); 
+    	
+    	// 符号なし整数
+    	UnsignedInteger unsignedInteger = unsignedInteger();
+    	
+    	return new Integer(sign, unsignedInteger);
+    }
+    
+    /**
+     * 符号
+     * 
+     */
+    public Sign sign() throws SyntaxException {
+    	// "SPLUS", "SMINUS"の判定
+    	if (isToken("SPLUS") || isToken("SMINUS")) {
+    		return new Sign(getLexical(tokenIndex));
+    	} else {
+    		return null;
+    	}
     }
     
     /**
@@ -153,6 +260,46 @@ public class LL1 {
      * 
      */
     public SubprogramDeclarationGroup subprogramDeclarationGroup() throws SyntaxException {
+    	// 副プログラム宣言
+    	SubprogramDeclaration subprogramDeclaration = subprogramDeclaration();
+    	
+    	// "SSEMICOLON"の判定
+    	checkToken("SSEMICOLON");
+    	
+    	return SubprogramDeclaration(subprogramDeclaration);
+    }
+    
+    /**
+     * 副プログラム宣言
+     * 
+     */
+    public SubprogramDeclaration subprogramDeclaration() throws SyntaxException {
+    	// 副プログラム頭部
+    	SubprogramHead subprogramHead = subprogramHead();
+    	
+    	// 変数宣言
+    	VariableDeclaration variableDeclaration = variableDeclaration(); 
+    	
+    	// 複合文
+    	ComplexStatement complexStatement = complexStatement();
+    	
+    	return new SubprogramDeclaration(subprogramHead, variableDeclaration, complexStatement);
+    }
+    
+    /**
+     * 副プログラム頭部
+     * 
+     */
+    public SubprogramHead subprogramHead() throws SyntaxException {
+    	// "SPROCEDURE"の判定
+    	checkToken("SPROCEDURE");
+    }
+    
+    /**
+     * 符号なし整数
+     * 
+     */
+    public UnsignedInteger unsignedInteger() throws SyntaxException {
     	
     }
 }
