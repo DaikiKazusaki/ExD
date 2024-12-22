8e15bfb03081c733673314f3a9767577e795d47d
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2024-12-22 11:32:07 +0900

    now editing SemanticValidationVisitor.jav

--- s4/compiler/SemanticValidationVisitor.java
+++ s4/compiler/SemanticValidationVisitor.java
@@ -15,7 +15,9 @@ public class SemanticValidationVisitor extends Visitor {
     }
     
     @Override
-    public void visit(ProgramName programName) {}
+    public void visit(ProgramName programName) {
+    	String name = programName.getProgramName();
+    }
     
     @Override
     public void visit(Block block) throws SemanticException {
@@ -34,92 +36,208 @@ public class SemanticValidationVisitor extends Visitor {
     }
     
     @Override
-    public void visit(VariableDeclarationGroup variableDeclarationGroup) {
+    public void visit(VariableDeclarationGroup variableDeclarationGroup) throws SemanticException {
     	List<VariableNameGroup> variableNameGroupList = variableDeclarationGroup.getVariableNameGroupList();
     	
-    	// variableNameGroupList.accept(this);
+    	for (VariableNameGroup variableNameGroup : variableNameGroupList) {
+            variableNameGroup.accept(this);
+        }
     }
     
     @Override
-    public void visit(VariableNameGroup variableNameGroup) {}
+    public void visit(VariableNameGroup variableNameGroup) throws SemanticException {
+    	List<VariableName> variableNameList = variableNameGroup.getVariableNameList();
+    	
+    	for (VariableName variableName: variableNameList) {
+    		variableName.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(VariableName variableName) {}
+    public void visit(VariableName variableName) {
+    	String name = variableName.getVariableName();
+    }
+    
     @Override
-    public void visit(Type type) {}
+    public void visit(Type type) throws SemanticException {
+    	StandardType standardType = type.getStandardType();
+    	ArrayType arrayType = type.getArrayType();
+    	
+    	if (standardType != null) {
+    		standardType.accept(this);
+    	} else if (arrayType != null) {
+    		arrayType.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(StandardType standardType) {}
+    public void visit(StandardType standardType) {
+    	String type = standardType.getStandardType();
+    }
+    
     @Override
-    public void visit(ArrayType arrayType) {}
+    public void visit(ArrayType arrayType) throws SemanticException {
+    	Int minimumInteger = arrayType.getMinimumInteger();
+    	Int maximumInteger = arrayType.getMinimumInteger();
+    	StandardType standardType = arrayType.getStandardType();
+    	
+    	minimumInteger.accept(this);
+    	maximumInteger.accept(this);
+    	standardType.accept(this);
+    }
+    
     @Override
-    public void visit(Int integer) {}
+    public void visit(Int integer) throws SemanticException {
+    	Sign sign = integer.getSign();
+    	UnsignedInteger unsignedInteger = integer.getUnsignedInteger();
+    	
+    	if (sign != null) {
+    		sign.accept(this);
+    	}
+    	unsignedInteger.accept(this);
+    }
+    
     @Override
-    public void visit(Sign sign) {}
+    public void visit(Sign sign) {
+    	String name = sign.getSign();
+    }
+    
     @Override
-    public void visit(SubprogramDeclarationGroup subprogramDeclarationGroup) {}
+    public void visit(SubprogramDeclarationGroup subprogramDeclarationGroup) throws SemanticException {
+    	List<SubprogramDeclaration> subprogramDeclarationList = subprogramDeclarationGroup.getSubprogramDeclaration();
+    	
+    	for (SubprogramDeclaration subprogramDeclaration: subprogramDeclarationList) {
+    		subprogramDeclaration.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(SubprogramDeclaration subprogramDeclaration) {}
+    public void visit(SubprogramDeclaration subprogramDeclaration) throws SemanticException {
+    	SubprogramHead subprogramHead = subprogramDeclaration.getSubprogramHead();
+    	VariableDeclaration variableDeclaration = subprogramDeclaration.getVariableDeclaration();
+    	ComplexStatement complexStatement = subprogramDeclaration.getComplexStatement();
+    	
+    	subprogramHead.accept(this);
+    	variableDeclaration.accept(this);
+    	complexStatement.accept(this);
+    }
+    
     @Override
-    public void visit(SubprogramHead subprogramHead) {}
+    public void visit(SubprogramHead subprogramHead) throws SemanticException {
+    	ProcedureName procedureName = subprogramHead.getProcedureName();
+    	FormalParameter formalParameter = subprogramHead.getFormalParameter();
+    	
+    	procedureName.accept(this);
+    	formalParameter.accept(this);
+    }
+    
     @Override
-    public void visit(ProcedureName procedureName) {}
+    public void visit(ProcedureName procedureName) {
+    	String name = procedureName.getProcedureName();
+    }
+    
     @Override
-    public void visit(FormalParameter formalParameter) {}
+    public void visit(FormalParameter formalParameter) throws SemanticException {
+    	FormalParameterGroup formalParameterGroup = formalParameter.getFormalParameterGroup();
+    	
+    	formalParameterGroup.accept(this);
+    }
+    
     @Override
-    public void visit(FormalParameterGroup formalParameterGroup) {}
+    public void visit(FormalParameterGroup formalParameterGroup) throws SemanticException {
+    	List<FormalParameterNameGroup> formalParameterNameGroupList = formalParameterGroup.getFormalParameterNameGroupList();
+    	List<StandardType> standardTypeList = formalParameterGroup.getStandardTypeList();
+    	
+    	for (int i = 0; i < formalParameterNameGroupList.size(); i++) {
+    		FormalParameterNameGroup formalParameterNameGroup = formalParameterNameGroupList.get(i);
+    		StandardType standardType = standardTypeList.get(i);
+    		
+    		formalParameterNameGroup.accept(this);
+    		standardType.accept(this);
+    	}
+    }
+    
     @Override
     public void visit(FormalParameterNameGroup formalParameterNameGroup) {}
+    
     @Override
     public void visit(FormalParameterName formalParameterName) {}
+    
     @Override
     public void visit(ComplexStatement complexStatement) {}
+    
     @Override
     public void visit(StatementGroup statementGroup) {}
+    
     @Override
     public void visit(Statement statement) {}
+    
     @Override
     public void visit(IfThen ifThen) {}
+    
     @Override
     public void visit(ElseStatement elseStatement) {}
+    
     @Override
     public void visit(WhileDo whileDo) {}
+    
     @Override
     public void visit(BasicStatement basicStatement) {}
+    
     @Override
     public void visit(AssignStatement assignStatement) {}
+    
     @Override
     public void visit(LeftSide leftSide) {}
+    
     @Override
     public void visit(Variable variable) {}
+    
     @Override
     public void visit(NaturalVariable naturalVariable) {}
+    
     @Override
     public void visit(VariableWithIndex variableWithIndex) {}
+    
     @Override
     public void visit(Index index) {}
+    
     @Override
     public void visit(ProcedureCallStatement procedureCallStatement) {}
+    
     @Override
     public void visit(EquationGroup equationGroup) {}
+    
     @Override
     public void visit(Equation equation) {}
+    
     @Override
     public void visit(SimpleEquation simpleEquation) {}
+    
     @Override
     public void visit(Term term) {}
+    
     @Override
     public void visit(Factor factor) {}
+    
     @Override
     public void visit(RelationalOperator relationalOperator) {}
+    
     @Override
     public void visit(AdditionalOperator additionalOperator) {}
+    
     @Override
     public void visit(MultipleOperator multipleOperator) {}
+    
     @Override
     public void visit(InputOutputStatement inputOutputStatement) {}
+    
     @Override
     public void visit(VariableGroup variableGroup) {}
+    
     @Override
     public void visit(Constant constant) {}
+    
     @Override
     public void visit(UnsignedInteger unsignedInteger) {}
 }
--- s4/compiler/Visitor.java
+++ s4/compiler/Visitor.java
@@ -1,24 +1,24 @@
 package enshud.s4.compiler;
 
 public abstract class Visitor {
-	public void visit(Program program) {}
+	public void visit(Program program) throws SemanticException {}
 	public void visit(ProgramName programName) {}
-	public void visit(Block block) {}
-	public void visit(VariableDeclaration variableDeclaration) {}
-	public void visit(VariableDeclarationGroup variableDeclarationGroup) {}
-	public void visit(VariableNameGroup variableNameGroup) {}
+	public void visit(Block block) throws SemanticException {}
+	public void visit(VariableDeclaration variableDeclaration) throws SemanticException {}
+	public void visit(VariableDeclarationGroup variableDeclarationGroup) throws SemanticException {}
+	public void visit(VariableNameGroup variableNameGroup) throws SemanticException {}
 	public void visit(VariableName variableName) {}
-	public void visit(Type type) {}
+	public void visit(Type type) throws SemanticException {}
 	public void visit(StandardType standardType) {}
-	public void visit(ArrayType arrayType) {}
-	public void visit(Int integer) {}
+	public void visit(ArrayType arrayType) throws SemanticException {}
+	public void visit(Int integer) throws SemanticException {}
 	public void visit(Sign sign) {}
-	public void visit(SubprogramDeclarationGroup subprogramDeclarationGroup) {}
-	public void visit(SubprogramDeclaration subprogramDeclaration) {}
-	public void visit(SubprogramHead subprogramHead) {}
+	public void visit(SubprogramDeclarationGroup subprogramDeclarationGroup) throws SemanticException {}
+	public void visit(SubprogramDeclaration subprogramDeclaration) throws SemanticException {}
+	public void visit(SubprogramHead subprogramHead) throws SemanticException {}
 	public void visit(ProcedureName procedureName) {}
-	public void visit(FormalParameter formalParameter) {}
-	public void visit(FormalParameterGroup formalParameterGroup) {}
+	public void visit(FormalParameter formalParameter) throws SemanticException {}
+	public void visit(FormalParameterGroup formalParameterGroup) throws SemanticException {}
 	public void visit(FormalParameterNameGroup formalParameterNameGroup) {}
 	public void visit(FormalParameterName formalParameterName) {}
 	public void visit(ComplexStatement complexStatement) {}
