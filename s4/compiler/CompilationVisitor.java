1586a9d33fd42e3c27491903b9fbf8c47c3a143a
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2024-12-23 11:21:22 +0900

    Finish adding accepter to CompilationVisitor

--- s4/compiler/CompilationVisitor.java
+++ s4/compiler/CompilationVisitor.java
@@ -158,57 +158,230 @@ public class CompilationVisitor extends SemanticValidationVisitor {
     
     @Override
     public void visit(FormalParameterName formalParameterName) {}
+    
     @Override
-    public void visit(ComplexStatement complexStatement) {}
+    public void visit(ComplexStatement complexStatement) throws SemanticException {
+    	StatementGroup statementGroup = complexStatement.getStatementGroup();
+    	
+    	statementGroup.accept(this);
+    }
+    
     @Override
-    public void visit(StatementGroup statementGroup) {}
+    public void visit(StatementGroup statementGroup) throws SemanticException {
+    	List<Statement> statementList = statementGroup.getStatementList();
+    	
+    	for (Statement statement: statementList) {
+    		statement.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(Statement statement) {}
+    public void visit(Statement statement) throws SemanticException {
+    	BasicStatement basicStatement = statement.getBasicStatement();
+    	IfThen ifThen = statement.getIfThen();
+    	WhileDo whileDo = statement.getWhileDo();
+    	
+    	if (basicStatement != null) {
+    		basicStatement.accept(this);
+    	} else if (ifThen != null) {
+    		ifThen.accept(this);
+    	} else if (whileDo != null) {
+    		whileDo.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(IfThen ifThen) {}
+    public void visit(IfThen ifThen) throws SemanticException {
+    	Equation equation = ifThen.getEquation();
+    	ComplexStatement complexStatement = ifThen.getComplexStatement();
+    	
+    	equation.accept(this);
+    	complexStatement.accept(this);
+    }
+    
     @Override
-    public void visit(ElseStatement elseStatement) {}
+    public void visit(ElseStatement elseStatement) throws SemanticException {
+    	ComplexStatement complexStatement = elseStatement.getComplexStatement();
+    	
+    	complexStatement.accept(this);
+    }
+    
     @Override
-    public void visit(WhileDo whileDo) {}
+    public void visit(WhileDo whileDo) throws SemanticException {
+    	Equation equation = whileDo.getEquation();
+    	ComplexStatement complexStatement = whileDo.getComplexStatement();
+    	
+    	equation.accept(this);
+    	complexStatement.accept(this);
+    }
+    
     @Override
-    public void visit(BasicStatement basicStatement) {}
+    public void visit(BasicStatement basicStatement) throws SemanticException {
+    	AssignStatement assignStatement = basicStatement.getAssignStatement();
+    	ProcedureCallStatement procedureCallStatement = basicStatement.getProcedureCallStatement();
+    	InputOutputStatement inputOutputStatement = basicStatement.getInputOutputStatement();
+    	ComplexStatement complexStatement = basicStatement.getComplexStatement();
+    	
+    	if (assignStatement != null) {
+    		assignStatement.accept(this);
+    	} else if (procedureCallStatement != null) {
+    		procedureCallStatement.accept(this);
+    	} else if (inputOutputStatement != null) {
+    		inputOutputStatement.accept(this);
+    	} else if (complexStatement != null) {
+    		complexStatement.accept(this);
+    	}
+    } 
+    
     @Override
-    public void visit(AssignStatement assignStatement) {}
+    public void visit(AssignStatement assignStatement) throws SemanticException {
+    	LeftSide leftSide = assignStatement.getLeftSide();
+    	Equation equation = assignStatement.getEquation();
+    	
+    	leftSide.accept(this);
+    	equation.accept(this);
+    }
+    
     @Override
-    public void visit(LeftSide leftSide) {}
+    public void visit(LeftSide leftSide) throws SemanticException {
+    	Variable variable = leftSide.getVariable();
+    	
+    	variable.accept(this);
+    }
+    
     @Override
-    public void visit(Variable variable) {}
+    public void visit(Variable variable) throws SemanticException {
+    	NaturalVariable naturalVariable = variable.getNaturalVariable();
+    	VariableWithIndex variableWithIndex = variable.getVariableWithIndex();
+    	
+    	if (naturalVariable != null) {
+    		naturalVariable.accept(this);
+    	} else if (variableWithIndex != null) {
+    		variableWithIndex.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(NaturalVariable naturalVariable) {}
+    public void visit(NaturalVariable naturalVariable) throws SemanticException {
+    	VariableName variableName = naturalVariable.getVariableName();
+    	
+    	variableName.accept(this);
+    }
+    
     @Override
-    public void visit(VariableWithIndex variableWithIndex) {}
+    public void visit(VariableWithIndex variableWithIndex) throws SemanticException {
+    	VariableName variableName = variableWithIndex.getVariableName();
+    	Index index = variableWithIndex.getIndex();
+    	
+    	variableName.accept(this);
+    	index.accept(this);
+    }
+    
     @Override
-    public void visit(Index index) {}
+    public void visit(Index index) throws SemanticException {
+    	Equation equation = index.getEquation();
+    	
+    	equation.accept(this);
+    }
+    
     @Override
-    public void visit(ProcedureCallStatement procedureCallStatement) {}
+    public void visit(ProcedureCallStatement procedureCallStatement) throws SemanticException {
+    	ProcedureName procedureName = procedureCallStatement.getProcedureName();
+    	EquationGroup equationGroup = procedureCallStatement.getEquationGroup();
+    	
+    	procedureName.accept(this);
+    	if (equationGroup != null) {
+    		equationGroup.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(EquationGroup equationGroup) {}
+    public void visit(EquationGroup equationGroup) throws SemanticException {
+    	List<Equation> equationList = equationGroup.getEquationList();
+    	
+    	for (Equation equation: equationList) {
+    		equation.accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(Equation equation) {}
+    public void visit(Equation equation) throws SemanticException {
+    	List<SimpleEquation> simpleEquationList = equation.getSimpleEquationList();
+    	List<RelationalOperator> relationalOperatorList = equation.getRelationalOperatorList();
+    	
+    	simpleEquationList.get(0).accept(this);
+    	for (int i = 0; i < relationalOperatorList.size(); i++) {
+    		relationalOperatorList.get(i).accept(this);
+    		simpleEquationList.get(i + 1).accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(SimpleEquation simpleEquation) {}
+    public void visit(SimpleEquation simpleEquation) throws SemanticException {
+    	Sign sign = simpleEquation.getSign();
+    	List<Term> termList = simpleEquation.getTermList();
+    	List<AdditionalOperator> additionalOperatorList = simpleEquation.getAdditionalOperatorList();
+    	
+    	if (sign != null) {
+    		sign.accept(this);
+    	} 
+    	termList.get(0).accept(this);
+    	for (int i = 0; i < additionalOperatorList.size(); i++) {
+    		additionalOperatorList.get(i).accept(this);
+    		termList.get(i + 1).accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(Term term) {}
+    public void visit(Term term) throws SemanticException {
+    	List<Factor> factorList = term.getFactorList();
+    	List<MultipleOperator> multipleOperatorList = term.getMultipleOperatorList();
+    	
+    	factorList.get(0).accept(this);
+    	for (int i = 0; i < multipleOperatorList.size(); i++) {
+    		multipleOperatorList.get(i).accept(this);
+    		factorList.get(i + 1).accept(this);
+    	}
+    }
+    
     @Override
-    public void visit(Factor factor) {}
+    public void visit(Factor factor) throws SemanticException {
+    	Variable variable = factor.getVariable();
+    	Constant constant = factor.getConstant();
+    	Equation equation = factor.getEquation();
+    	Factor notFactor = factor.getFactor();
+    	
+    	if (variable != null) {
+    		variable.accept(this);
+    	} else if (constant != null) {
+    		constant.accept(this);
+    	} else if (equation != null) {
+    		equation.accept(this);
+    	} else if (notFactor != null) {
+    		notFactor.accept(this);
+    	}
+    }
+    
     @Override
     public void visit(RelationalOperator relationalOperator) {}
+    
     @Override
     public void visit(AdditionalOperator additionalOperator) {}
+    
     @Override
     public void visit(MultipleOperator multipleOperator) {}
     
     @Override
-    public void visit(InputOutputStatement inputOutputStatement) {
-    	if (inputOutputStatement.getVariableGroup() != null) {
-    		// "readln"の場合
-    	} else if (inputOutputStatement.getEquationGroup() != null) {
-    		// "writeln"の場合
+    public void visit(InputOutputStatement inputOutputStatement) throws SemanticException {
+    	VariableGroup variableGroup = inputOutputStatement.getVariableGroup();
+    	EquationGroup equationGroup = inputOutputStatement.getEquationGroup();
+    	
+    	if (variableGroup != null) {
+    		// readlnの場合
+    		variableGroup.accept(this);
+    	} else if (equationGroup != null) {
+    		// writelnの場合
+    		equationGroup.accept(this);
     		writeln();
     	}
     }
@@ -250,9 +423,17 @@ public class CompilationVisitor extends SemanticValidationVisitor {
     }
     
     @Override
-    public void visit(VariableGroup variableGroup) {}
+    public void visit(VariableGroup variableGroup) throws SemanticException {
+    	List<Variable> variableList = variableGroup.getVariableList();
+    	
+    	for (Variable variable: variableList) {
+    		variable.accept(this);
+    	}
+    }
+    
     @Override
     public void visit(Constant constant) {}
+    
     @Override
     public void visit(UnsignedInteger unsignedInteger) {}
 }
